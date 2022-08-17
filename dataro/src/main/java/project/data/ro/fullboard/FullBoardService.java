7b115fbc7d4a1eddd184861e310e830035e7516e
dlghd <dlghd@DESKTOP-92U4QK9>
2022-08-17 10:43:35 +0900

    호윤 첫 커밋

---
+++ dataro/src/main/java/project/data/ro/fullboard/FullBoardService.java
@@ -0,0 +1,29 @@
+package project.data.ro.fullboard;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public interface FullBoardService {
+	
+	
+	Map index(FullBoardVO vo); //목록게시판
+	
+	int insert(FullBoardVO vo); //등록
+	
+	FullBoardVO view(FullBoardVO vo); //상세게시판
+	
+	FullBoardVO modify(FullBoardVO vo); // 수정폼
+	
+	boolean remove(int num); //삭제
+	
+	boolean update(FullBoardVO vo); //수정기능
+	
+	boolean fileUpload(FullBoardVO vo, MultipartFile[] filename, HttpServletRequest req); // 파일등록
+
+	
+}

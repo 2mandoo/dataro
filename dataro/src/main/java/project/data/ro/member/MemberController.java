e057a07ae24b65475393e513a7aa7aa9ff8ce5a0
Jeong Gil <withyouatw@gmail.com>
2022-08-17 15:47:09 +0900

    ojg pushed

--- dataro/src/main/java/project/data/ro/member/MemberController.java
+++ dataro/src/main/java/project/data/ro/member/MemberController.java
@@ -297,8 +297,10 @@ public class MemberController {
 			vo.setMessage_no(message_no);
 			service.readUpdate(vo);
 		}
-		model.addAttribute("msg",  "읽음처리가 완료되었습니다.");
-		model.addAttribute("url", "/ro/board/main.do");
+		/*
+		 * model.addAttribute("msg", "읽음처리가 완료되었습니다."); model.addAttribute("url",
+		 * "/ro/board/main.do");
+		 */
 		return "common/alert";
 	}
 	
